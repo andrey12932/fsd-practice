3b05a7f113f24d1c5c1156865d92bdb817f760b0
lomovav <lomovav@63pokupki.ru>
2023-07-29 21:18:28 +0400

    i18n translation

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const MainPage = () => {
+    const {t} = useTranslation("main");
+
     return (
         <div className="">
-            MAIN PAGE
+            {t('Главная страница')}
         </div>
     );
 }
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {classNames} from "shared/lib/classNames";
 import cls from './Sidebar.module.scss';
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
     className?: string
@@ -19,6 +20,7 @@ const Sidebar = ({className}: SidebarProps) => {
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
